5e147b5b73308d15925556c584799806161ade20
usama-govsoft <robert@govsoft.us>
2021-04-30 10:52:05 +0500

    using github actions to deploy site

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,61 +1,6 @@
 // import React, { useEffect } from 'react'
 import './login.scss'
-
-
-// const Login = ({ history }) => {
-//     const working = () => {
-//         var head = document.getElementsByTagName('head')[0]
-//         var script = document.createElement('script')
-//         script.defer = 1
-//         script.src = "resources/login.js"
-//         head.appendChild(script)
-//     }
-//     useEffect(() => {
-//         working()
-//     }, [])
-//     const signUpUser = (e) => {
-//         e.preventDefault()
-//         history.push("/dashboard")
-//     }
-//     const loginUser = (e) => {
-//         e.preventDefault()
-//         history.push("/dashboard")
-//     }
-//     return (
-//         <div className='parent_login'>
-//             <div class="form-structor">
-//                 <form onSubmit={signUpUser} >
-//                     <div class="signup">
-//                         <h2 class="form-title" id="signup"><span>or</span>Sign up</h2>
-//                         <div class="form-holder">
-//                             <input type="text" class="input" placeholder="Name" />
-//                             <input type="email" class="input" placeholder="Email" />
-//                             <input type="password" class="input" placeholder="Password" />
-//                         </div>
-//                         <button type='submit' class="submit-btn">Sign up</button>
-//                     </div>
-//                 </form>
-//                 <form onSubmit={loginUser} >
-//                     <div class="login slide-up">
-//                         <div class="center">
-//                             <h2 class="form-title" id="login"><span>or</span>Log in</h2>
-//                             <div class="form-holder">
-//                                 <input type="email" class="input" placeholder="Email" />
-//                                 <input type="password" class="input" placeholder="Password" />
-//                             </div>
-//                             <button type='submit' class="submit-btn">Log in</button>
-//                         </div>
-//                     </div>
-//                 </form>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Login
-
 import React, { Component } from 'react'
-
 export default class Login extends Component {
 
     working = () => {
@@ -63,6 +8,8 @@ export default class Login extends Component {
         var script = document.createElement('script')
         script.defer = 1
         script.src = "resources/login.js"
+        // script.src = ""
+
         head.appendChild(script)
     }
 
@@ -106,6 +53,7 @@ export default class Login extends Component {
                         </div>
                     </form>
                 </div>
+                {/* <AdsensComponent /> */}
             </div>
         )
     }
---
+++ src/Components/TestAds.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react"
+import AdSense from "react-adsense"
+import { render } from "react-dom"
+
+
+class BasicAd extends Component {
+    componentDidMount() {
+        (window.adsbygoogle = window.adsbygoogle || []).push({})
+    }
+
+    render() {
+        return (
+            <ins
+                className="adsbygoogle"
+                data-ad-client="ca-pub-4591861188995436"
+                data-ad-slot="4640466102"
+                style={{ display: "inline-block", height: 250, width: 300 }}
+            />
+        )
+    }
+}
+
+export const TestAd = () => (
+
+    <BasicAd />
+
+
+)
+
+render(<TestAd />, document.getElementById("root"))
